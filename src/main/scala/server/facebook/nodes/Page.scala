c17872f3dab66296fac3dcd1292ed87cae48421e
Joji Jacob <jojijacob06@gmail.com>
2015-11-17 22:54:34 -0500

    Update User and Page node

--- src/main/scala/server/facebook/nodes/Page.scala
+++ src/main/scala/server/facebook/nodes/Page.scala
@@ -1,5 +1,19 @@
 package server.facebook.nodes
 
-class Page {
+class Page(var _id:String,
+            var _about:String,
+            var _description:String,
+            var _likes:Integer) {
 
+  // setters
+  def id = _id
+  def about = _about
+  def description = _description
+  def likes = _likes
+
+  //getters
+  def id_=(idIn: String): Unit = _id = idIn
+  def about_=(aboutIn: String): Unit = _about = aboutIn
+  def description_=(descriptionIn: String): Unit = _description = descriptionIn
+  def likes_=(likesIn: Integer): Unit = _likes = likesIn
 }
--- src/main/scala/server/facebook/nodes/User.scala
+++ src/main/scala/server/facebook/nodes/User.scala
@@ -1,5 +1,22 @@
 package server.facebook.nodes
 
-class User {
+class User(var _id:String,
+            var _about:String,
+            var _birthday:String,
+            var _email:String,
+            var _first_name:String) {
 
+  // setters
+  def id = _id
+  def about = _about
+  def birthday = _birthday
+  def email = _email
+  def first_name = _first_name
+
+  // getters
+  def id_=(idIn: String): Unit = _id = idIn
+  def about_=(aboutIn: String): Unit = _about = aboutIn
+  def birthday_=(birthdayIn: String): Unit = _birthday = birthdayIn
+  def email_=(emailIn: String): Unit = _email = emailIn
+  def first_name_=(first_nameIn: String): Unit = _first_name = first_nameIn
 }
